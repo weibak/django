0acb3dd522563364bd94dcdb22b6267f6d07fa9b
Artem Sheibak <sheibakaa@gmail.com>
2022-01-17 19:30:39 +0300

    add teachers homework

--- blog/photos/views.py
+++ blog/photos/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
-import logging
 
+import logging
+from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
 
 
-def blog1_index(request):
-    value = request.GET.get("key_1")
-    logger.info(value)
-    return HttpResponse("Blog_1 index view")
+def profiles_index(request):
+    # Processing GET params
+    value = request.GET.get("get-key-1")
+    value and logger.info(f"get-key-1 = {value}")
+    value = request.GET.get("get-key-2")
+    value and logger.info(f"get-key-2 = {value}")
+    value = request.GET.get("get-key-3")
+    value and logger.info(f"get-key-3 = {value}")
+
+    # Processing POST params
+    value = request.POST.get("post-key-1")
+    value and logger.info(f"post-key-1 = {value}")
+    value = request.POST.get("post-key-2")
+    value and logger.info(f"post-key-2 = {value}")
+    value = request.POST.get("post-key-3")
+    value and logger.info(f"post-key-3 = {value}")
+
+    return HttpResponse("Profiles index view")
 
 
 def blog1_index_2(request):
---
+++ blog/tests/test_profiles.py
@@ -0,0 +1,22 @@
+
+from django.test import Client
+
+
+class TestProfiles:
+    def test_profiles_index_view(self):
+        client = Client()
+
+        response = client.get("/profiles/")
+        assert response.status_code == 200
+
+        response = client.get("/profiles/?get-key-1=test-key-1")
+        assert response.status_code == 200
+
+        response = client.get("/profiles/?get-key-1=test-key-1&get-key-2=test-key-2&get-key-3=test-key-3")
+        assert response.status_code == 200
+
+        response = client.post("/profiles/?get-key-1=test-key-1", data={"post-key-2": "test-key-2"})
+        assert response.status_code == 200
+
+        response = client.post("/profiles/", data={"post-key-1": "test-key-1", "post-key-3": "test-key-2"})
+        assert response.status_code == 200
