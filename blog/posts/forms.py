6787ff56b658cbede9dc9301824387354f134ce6
Artem Sheibak <sheibakaa@gmail.com>
2022-01-26 15:44:19 +0300

    minor

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class PostForm(forms.Form):
     title = forms.CharField(max_length=50)
     text = forms.CharField(max_length=200)
-    image = forms.ImageField()
+    image = forms.ImageField(required=False)
     slug = forms.CharField(max_length=6)
