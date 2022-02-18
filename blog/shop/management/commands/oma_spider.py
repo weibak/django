a9592b0d4c11bf6c29d23036a84377a56aafe3d0
Artem Sheibak <sheibakaa@gmail.com>
2022-02-18 20:29:52 +0300

    update spiders

--- blog/shop/management/commands/oma_spider.py
+++ blog/shop/management/commands/oma_spider.py
@@ -1,5 +1,8 @@
 import logging
+from pathlib import Path
 
+import requests
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from shop.models import Product
@@ -19,6 +22,12 @@ class Command(BaseCommand):
 
         def crawler_results(signal, sender, item, response, spider):
             item["cost"] = int(item["cost"].split(",")[0])
+            if item["image"]:
+                response = requests.get(item["image"])
+                if response.ok:
+                    path = Path(item["image"])
+                    open(settings.MEDIA_ROOT / path.name, "wb").write(response.content)
+                    item["image"] = path.name
             Product.objects.update_or_create(external_id=item["external_id"], defaults=item)
 
         dispatcher.connect(crawler_results, signal=signals.item_scraped)
--- blog/shop/spiders.py
+++ blog/shop/spiders.py
@@ -1,18 +1,11 @@
 import logging
-from pathlib import Path
 
 import scrapy
-import logging
-import requests
-
-from blog import settings
-
-logger = logging.getLogger(__name__)
 
 
 class OmaSpider(scrapy.Spider):
     name = "oma.by"
-    allowed_domains = ["https://www.oma.by"]
+    allowed_domains = ["www.oma.by"]
     start_urls = ["https://www.oma.by/elektroinstrument-c"]
 
     def __init__(self, *args, **kwargs):
@@ -21,33 +14,22 @@ class OmaSpider(scrapy.Spider):
         super().__init__(*args, **kwargs)
 
     def parse(self, response, **kwargs):
-        logger.info("processing:"+response.url)
         for product in response.css(".catalog-grid .product-item"):
-            external_id = int(product.css("::attr(data-ga-product-id)").get()),
-            image_link = "https://oma.by/" + str(product.css('.product-item_img-box img:first-child::attr(data-lazy)').get())
-            filename = Path(settings.BASE_DIR / f"{external_id}.jpg")
-            responsee = requests.get(image_link)
-            filename.write_bytes(responsee.content)
+            image_link = product.css('.product-item_img-box img:first-child::attr(data-lazy)').get()
             if not image_link:
-                external_id = int(product.css("::attr(data-ga-product-id)").get()),
-                image_link = "https://oma.by/" + str(product.css('.product-item_img-box img:first-child::attr(data-src)').get())
-                filename = Path(settings.BASE_DIR / f"{external_id}.jpg")
-                responsee = requests.get(image_link)
-                filename.write_bytes(responsee.content)
+                image_link = product.css(".product-item_img-box img:first-child::attr(data-src)").get()
+            price = product.css(".product-price-block .price__normal::text").get()
+            price = price.strip() if price is not None else 0
             data = {
-                "external_id": f"{self.allowed_domains[0]}{external_id}",
+                "external_id": int(product.css("::attr(data-ga-product-id)").get()),
                 "title": product.css(".product-title-and-rate-block .wrapper::text").get().strip(),
-                "cost": product.css(".product-price-block .price__normal::text").get().strip(),
-                "link": f"{self.allowed_domains[0]}{product.css('a.area-link::attr(href)').get()}",
-                "image": f"{self.allowed_domains[0]}{image_link}",
+                "cost": price,
+                "link": f"https://{self.allowed_domains[0]}{product.css('a.area-link::attr(href)').get()}",
+                "image": f"https://{self.allowed_domains[0]}{image_link}",
+                "status": "IN_STOCK" if price else "OUT_OF_STOCK",
             }
             yield data
 
-        NEXT_PAGE_SELECTOR = '.btn__nav-right::attr(href)'
-        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-        if next_page:
-            logger.info({next_page})
-            yield scrapy.Request(
-                response.urljoin(next_page),
-                callback=self.parse
-                )
+        next_page = response.css(".page-nav_box .btn__nav-right::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
